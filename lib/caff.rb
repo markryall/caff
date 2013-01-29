6c6de2c40ad31402f91b00892b1cd6085e52f1a7
Mark Ryall <mark@ryall.name>
2013-01-30 08:54:40 +1000

    moved everything out of bin script into a mixin

--- lib/caff.rb
+++ lib/caff.rb
@@ -1,5 +1,19 @@
-require "caff/version"
-
 module Caff
-  # Your code goes here...
+  def execute on_off
+    if %w{on off}.include? on_off
+      tell_caffeine "turn #{on_off}"
+    else
+      puts 'usage: caff [on|off]'
+    end
+    show_status
+  end
+
+  def show_status
+    current = tell_caffeine('active') == 'true' ? 'on' : 'off'
+    puts "caffeine is now #{current}"
+  end
+
+  def tell_caffeine command
+    `osascript -e 'tell application \"Caffeine\" to #{command}'`.chomp
+  end
 end
--- lib/caff/version.rb
+++ lib/caff/version.rb
@@ -1,3 +1,3 @@
 module Caff
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
